6a9ae80e0ea9ec264ce6034ee5be563be5d47204
jmininel <ju.mininel@hotmail.com>
2021-06-24 18:25:17 -0300

    rote and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { NewRoom } from './pages/NemRoom';
+import { BrowserRouter,Route } from 'react-router-dom'
+
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <>
-    <NewRoom />
-   </>
+    <BrowserRouter>
+    <Route path="/" exact component={Home} />
+    <Route path="/rooms/new" component={NewRoom} />
+
+    
+    </BrowserRouter>
   );
 }
 
