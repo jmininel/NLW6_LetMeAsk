5cb8cb2b1672e0fac498b5ac629c88151a371d50
jmininel <ju.mininel@hotmail.com>
2021-06-24 16:53:05 -0300

    scss class

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
+ import {ButtonHTMLAttributes} from 'react'
 
- export function Button() {
-//  let counter = 0;
-const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter+1);
-  }
-
-    
-
+ import '../styles/button.scss'
+ 
+ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+ 
+ 
+ 
+ export function Button(props: ButtonProps) {
     return (
-     <button onClick={increment}>
-         {counter}
-       </button>
+     <button className="button" {...props} />
+        
+      
     );
 }
